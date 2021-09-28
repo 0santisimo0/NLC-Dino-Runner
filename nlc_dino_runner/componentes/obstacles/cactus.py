fe65aef42ce282977ae8ff9ab1c0640010255f17
0santisimo0 <santicm95386b@gmail.com>
2021-09-28 08:53:27 -0400

    Peoyecto Final (Tarea Grupal)
    Santiago Caballero M.

--- nlc_dino_runner/componentes/obstacles/cactus.py
+++
@@ -1,9 +0,0 @@
-from nlc_dino_runner.componentes.obstacles.obstacles import Obstacles
-import random
-
-
-class Cactus(Obstacles):
-    def __init__(self, image):
-        self.type = random.randint(0, 2)
-        super().__init__(image, self.type)
-        self.rect.y = 320
--- nlc_dino_runner/main.py
+++ nlc_dino_runner/main.py
@@ -1,4 +1,4 @@
-from nlc_dino_runner.componentes.game import Game
+from nlc_dino_runner.components.game import Game
 
 if __name__ == '__main__':
     game = Game()
